ecf78003fbf1c349b7c80b0e750e9dadd19e2852
Дмитрий Ашмарин <101218004+wCotito@users.noreply.github.com>
2024-04-05 18:20:53 +0300

    update types

--- types/libHelper.d.ts
+++ types/libHelper.d.ts
@@ -1,5 +1,5 @@
 
- interface libHelper{
+interface libHelper {
   TgBot: new (token: string) => TgBot;
   /**
    * The BigQuery service.
@@ -8,17 +8,42 @@
   sheetService: new (sheet: GoogleAppsScript.Spreadsheet.Sheet) => SheetServiceInterface;
   scriptProcessesService: new (scriptId: string, OAuthToken: string) => ScriptProcessesServiceInterface;
 }
+// Формат данных которы  должен быть отправлен методу updateData в BigQueryService 
+interface dataForDB {
+  [key: string]: object[]
+}
 
+type sheetKeysObj = {
+  col: {};
+  row: {};
+}
+
+interface buildOptions {
+  where?: {
+    column: string
+    value: string
+    or?: [{ column: string, value: string }]
+    and?: [{ column: string, value: string }]
+  }
+  limit?: number
+  orderBy?: {
+    col: string
+    asc?: boolean
+  }
+
+}
 
 interface BigQueryService {
   projectId: string
   datasetId: string
+  ManageDatasets(): new (projectId: string) => ManageDatasetInterface
   sendQuery(query: string): GoogleAppsScript.BigQuery.Schema.QueryResponse
   getData(query: string): {}[]
   updateData(data: dataForDB): undefined | Error
   buildInserts(table: string, tableData: object[], datasetId: string): string
   buildUpdate(table: any, tableData: any, datasetId?: any): string
-  buildDelete(table: string, options?: object, datasetId?: string): string
+  buildDelete(table: string, options?: buildOptions, datasetId?: string): string
+  buildSelect(table: string, fields: any[], options?: buildOptions, datasetId?: string): string
   clearTable(table: string, datasetId?: string): any
 }
 
@@ -52,7 +77,7 @@ interface SheetServiceInterface {
    * статический метод для запуска и рсиановки лоадера
    * @returns { close: (msg: string) => void, open: (msg: string) => void}
    */
-  static loader(): {
+  loader(): {
     close: (msg: string) => void,
     open: (msg: string) => void,
     alert: (title: string, content: string) => void
@@ -74,20 +99,183 @@ interface ScriptProcessesServiceInterface {
    */
   getFuncRunningProcesses(funcName: string): any;
 
-   /**
-     * id скрипта
-     * @type {string}
-     */
+  /**
+    * id скрипта
+    * @type {string}
+    */
   scriptId: string;
-   /**
-    * токен аунтефикации
-    * @type {string}
-    */
-   OAuthToken: string;
+  /**
+   * токен аунтефикации
+   * @type {string}
+   */
+  OAuthToken: string;
 
-   /**
-    * url для запросов
-    * @type {string}
-    */
-   url: string;
+  /**
+   * url для запросов
+   * @type {string}
+   */
+  url: string;
 }
+
+interface OptParams {
+  labels?: object;
+  location?: string;
+  selfLink?: string;
+  description?: string;
+  access?: GoogleAppsScript.BigQuery.Schema.DatasetAccess[];
+}
+
+interface ManageDatasetInterface {
+
+  /**
+   * Создает датасет
+   * @param {string} datasetId - id датасета
+   * @param {string} projectId - id проекта
+   * @param {OptParams} optParams - дополнительные параметры
+   */
+  createDataset(datasetId: string, projectId?: string, optParams?: OptParams): void;
+
+  /**
+   * Получает список датасетов
+   * @param {string} projectId - id проекта
+   * @returns {BigQuery.Schema.DatasetList}
+   */
+  getDatasetsList(projectId?: string): GoogleAppsScript.BigQuery.Schema.DatasetListDatasets[];
+
+  /**
+   * Создает уникальный id для датасета
+   * @returns {string}
+   */
+  createUniqueDatasetId(): string;
+
+  /**
+   * Удаляет датасет
+   * @param {string} datasetId - id датасета
+   * @param {boolean} forse - флаг, если true, то удаляет датасет даже если в нем есть таблицы. По умолчанию false
+   * @param {string} projectId - id проекта
+   */
+  deleteDataset(datasetId: string, forse: boolean, projectId?: string): void;
+  /**
+   * Создает новый датасет с уникальным именем
+   * @returns {BigQuery.Schema.Dataset}
+   */
+  createNewDataset(): any;
+
+  createUpsertProcedure(datasetId: any, projectId?: any): GoogleAppsScript.BigQuery.Schema.Job;
+
+  updateSchema(schema: string, datasetId: any, projectId?: any): GoogleAppsScript.BigQuery.Schema.QueryResponse;
+
+  /**
+   * Обновляет доступы к датасету
+   * @param datasetId 
+   * @param accesses - массив объектов с параметрами доступа к датасету
+   * @param projectId 
+   */
+  updateAccess(datasetId: string, accesses: UpdateAccessOptions[], projectId?: string): GoogleAppsScript.BigQuery.Schema.Dataset;
+}
+
+interface UpdateAccessOptions {
+  role: string;
+  isAccess: boolean;
+  userByEmail?: string;
+  groupByEmail?: string;
+  domain?: string;
+  specialGroup?: string;
+  iamMember?: string;
+  view?: TableReference;
+  routine?: RoutineReference;
+  dataset?: DatasetAccessEntry;
+}
+
+interface FileOptions {
+  email: string;
+  fileId: string;
+  access: boolean;
+  accessType: "VIEW" | "EDIT" | "COMMENT";
+}
+
+interface GoogleDriveService {
+  /**
+   * Управление доступами к файлам
+   * @param email - email пользователя
+   * @param options - массив объектов с параметрами доступа к файлам
+   */
+  managePermision(options: FileOptions[]): void;
+
+  /**
+   * Метод устарел, используйте managePermision
+   * Добавляет доступ к файлу и библиотеке
+   * @deprecated
+   * @param email - email пользователя
+   * @param fileId - id файла
+   */
+  addPermision(email: string, fileId: string): void;
+
+  /**
+   * Метод устарел, используйте managePermision
+   * Забирает доступ к файлу и библиотеке
+   * @deprecated
+   * @param email - email пользователя
+   * @param removeLib - убрать доступ к библиотеке
+   * @param fileId - id файла
+   */
+  removePermision(email: string, removeLib?: boolean, fileId?: string): void;
+}
+
+
+/**
+ * Сервис для работы с ролями в Cloud Google Project. Для работы сервиса проект Apps Script должен быть привязан к проекту в GCP.
+ * В проекте GCP должен быть включен API - "Cloud Resource Manager API"(https://console.cloud.google.com/apis/library/cloudresourcemanager.googleapis.com);
+ * Настроены https://console.cloud.google.com/apis/api/cloudresourcemanager.googleapis.com/credentials OAuth 2.0 Client IDs;
+ * Создано и опубликовано приожение в https://console.cloud.google.com/apis/credentials/consent OAuth consent screen(Publishing status - In production);
+ * И иметь "oauthScopes": ["https://www.googleapis.com/auth/cloud-platform"]
+ * 
+ * P.S. Если кто найдет вариант получения токена аунтефикации GCP не через ScriptApp.getOAuthToken(), то пожалуйста напишите мне.
+ */
+interface ICgpRoleService {
+  projectId: string;
+  muteHttpExceptions: boolean;
+  OAuthToken: string;
+  imPolicyData: any;
+  /**
+   * Получает текущий список ролей для проекта(IamPolicy)
+   * Подробности - https://cloud.google.com/resource-manager/reference/rest/v3/projects/getIamPolicy
+   * @returns текущий список ролей для проекта
+   */
+  fetchImPolicy(): any;
+
+  /**
+   * Добавляет роль для пользователя
+   * @param email - email пользователя
+   * @param role - роль, которую необходимо добавить. For example, roles/viewer, roles/editor, or roles/owner. Подробнее тут - https://cloud.google.com/iam/docs/understanding-roles
+   */
+  addRole(email: string, role: string): void;
+
+  /**
+   * Удаляет роль для пользователя
+   * @param email - email пользователя
+   * @param role - роль, которую необходимо удалить. For example, roles/viewer, roles/editor, or roles/owner. Подробнее тут - https://cloud.google.com/iam/docs/understanding-roles
+   */
+  removeRole(email: string, role: string): void;
+
+  /**
+   * Обновляет политику доступа
+   * Подробности - https://cloud.google.com/resource-manager/reference/rest/v3/projects/setIamPolicy
+   * 
+   * Для того чтобы пользователь смог сохранить политики доступа, ему необходимо иметь роль минимум "roles/resourcemanager.projectIamAdmin" в проекте
+   * и он должен принять политику использования гугл клайд сервисов перейдя по ссылке  https://console.cloud.google.com/
+   */
+  updateImPolicy(): void;
+}
+
+type TableReference = { "projectId": string; "datasetId": string; "tableId": string; };
+
+type RoutineReference = {
+  "projectId": string;
+  "datasetId": string;
+  "routineId": string;
+};
+
+type DatasetReference = { datasetId: string; "projectId": string; };
+
+type DatasetAccessEntry = { dataset: DatasetReference; targetTypes: "VIEWS" | "TARGET_TYPE_UNSPECIFIED"; };
